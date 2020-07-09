81fb378ce065895e3ebf08b478ce6e94c6d35112
Greg <gkatechis@zendesk.com>
2020-07-09 16:40:53 -0500

    Updating req.body

--- nodejs/index.js
+++ nodejs/index.js
@@ -36,7 +36,7 @@ app.post('/messages', function(req, res) {
 
   if ((x.trigger === 'message:appUser') && (x.messages[0].source.type === 'messenger')) {
 
-    console.log('This is app.body:\n ' + x.messages)  
+    console.log('This is req.body:\n ' + x)  
     smooch.appUsers.sendMessage(appUserId, {
           type: 'text',
           text: 'Live long and prosper',
