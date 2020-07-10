92a51edf2d7a2dd612562e467c501952866249f2
Gregorios Katechis <gkatechis@zendesk.com>
2020-07-10 09:12:02 -0500

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -8,7 +8,7 @@ const Smooch = require('smooch-core');
 // Config
 const PORT = 80;
 const KEY_ID = 'app_5f04d0d83dfc64000c143f7c';
-const SECRET = API_SECRET;
+const SECRET = 4iMCHWOaJnZcfGAn3ulthRl2WCY5QRGxrt5pq23v-2ZgZVt1QVk0NS2TT3FfArA1WMw-dgXxJcrt0q7gnMdAPw;
 
 const smooch = new Smooch({
     keyId: KEY_ID,
